83718cdeaa6e42a1e9f04a5d2238d640d2956b78
unknown <konrad.bienkowski1@o2.pl>
2023-10-16 21:33:32 +0200

    smekma

---
+++ ConsoleApplicationzddom_arek/ConsoleApplicationzddom_arek/ConsoleApplicationzddom_arek.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+#include <math.h>
+using namespace std;
+int main()
+{
+    cout << "Hello World!";
+
+	int tablica[10] = { 2, 4, 6, 8, 10, 12, 14, 16, 18, 20 };
+    float tablica2[10] = { 1, 3, 5, 7, 9, 11, 13, 15, 17, 19 };
+	
+
+	cout << tablica[10] + tablica2[10] << endl;
+
+	return 0;
+}
+
